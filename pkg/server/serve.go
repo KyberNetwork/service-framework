217018fd38bb5280bb31ec85b6d55458f46cd386
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2023-12-20 18:16:28 +0700

    chore: lint fix
    - update deprecated usage of otelgrpc interceptors

--- pkg/server/serve.go
+++ pkg/server/serve.go
@@ -15,12 +15,14 @@ import (
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/selector"
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/validator"
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc/filters"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	_ "google.golang.org/grpc/encoding/gzip"
 	healthv1 "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/stats"
 	"google.golang.org/grpc/status"
 
 	"github.com/KyberNetwork/service-framework/pkg/metric"
@@ -42,6 +44,16 @@ func Serve(cfg *grpcserver.Config, services ...grpcserver.Service) {
 		logger.Fatalf("Serve|logger.InitLogger failed|err=%v", err)
 	}
 
+	isDevMode := grpcserver.GetAppMode(cfg.Mode) == grpcserver.Development
+
+	unaryOpts := []grpc.UnaryServerInterceptor{
+		grpcerror.UnaryServerInterceptor(isDevMode, internalServerErr),
+		client.UnaryServerInterceptor(),
+	}
+	var streamOpts []grpc.StreamServerInterceptor
+
+	logInterceptor := logmiddleware.Logger(log)
+	loggingOpts := getLoggingOptions(cfg.Flag.GRPC)
 	recoveryOpt := recovery.WithRecoveryHandler(func(err any) error {
 		logger.WithFields(logger.Fields{"error": err}).Errorf("recovered from:\n%s", string(debug.Stack()))
 		ctx, cancel := context.WithTimeout(context.Background(), pushPanicMetricTimeout)
@@ -49,27 +61,6 @@ func Serve(cfg *grpcserver.Config, services ...grpcserver.Service) {
 		metric.IncPanicTotal(ctx)
 		return internalServerErr
 	})
-
-	isDevMode := grpcserver.GetAppMode(cfg.Mode) == grpcserver.Development
-
-	unaryOpts := []grpc.UnaryServerInterceptor{
-		grpcerror.UnaryServerInterceptor(isDevMode, internalServerErr),
-		client.UnaryServerInterceptor(),
-	}
-	otelGRPCOptions := getOTELGRPCOptions()
-
-	var streamOpts []grpc.StreamServerInterceptor
-
-	if isEnabledOTEL() {
-		streamOpts = append(streamOpts, otelgrpc.StreamServerInterceptor(
-			otelGRPCOptions...,
-		))
-		unaryOpts = append(unaryOpts, otelgrpc.UnaryServerInterceptor(
-			otelGRPCOptions...,
-		))
-	}
-	loggingOpts := getLoggingOptions(cfg.Flag.GRPC)
-	logInterceptor := logmiddleware.Logger(log)
 	streamOpts = append(streamOpts,
 		selector.StreamServerInterceptor(logging.StreamServerInterceptor(logInterceptor,
 			loggingOpts...), selector.MatchFunc(healthSkip)),
@@ -82,10 +73,15 @@ func Serve(cfg *grpcserver.Config, services ...grpcserver.Service) {
 		recovery.UnaryServerInterceptor(recoveryOpt),
 	)
 
-	s := grpcserver.NewServer(cfg, isDevMode,
+	serverOptions := []grpc.ServerOption{
 		grpc.ChainUnaryInterceptor(unaryOpts...),
 		grpc.ChainStreamInterceptor(streamOpts...),
-	)
+	}
+	if isEnabledOTEL() {
+		otelGrpcStatHandler := getOtelGrpcStatsHandler()
+		serverOptions = append(serverOptions, grpc.StatsHandler(otelGrpcStatHandler))
+	}
+	s := grpcserver.NewServer(cfg, isDevMode, serverOptions...)
 
 	if err := s.Register(services...); err != nil {
 		logger.Fatalf("Error register servers %v", err)
@@ -105,26 +101,14 @@ func healthSkip(_ context.Context, c interceptors.CallMeta) bool {
 	return c.FullMethod() != healthv1.Health_Check_FullMethodName
 }
 
-func filterTrace() otelgrpc.Filter {
-	return func(ii *otelgrpc.InterceptorInfo) bool {
-		if ii != nil && isHealthCheckCall(ii) {
-			return false
-		}
-		return true
-	}
-}
-
-func isHealthCheckCall(ii *otelgrpc.InterceptorInfo) bool {
-	return ii.UnaryServerInfo != nil && ii.UnaryServerInfo.FullMethod == healthv1.Health_Check_FullMethodName || ii.StreamServerInfo != nil && ii.StreamServerInfo.FullMethod == healthv1.Health_Check_FullMethodName
-}
-
-func getOTELGRPCOptions() []otelgrpc.Option {
-	return []otelgrpc.Option{
-		otelgrpc.WithInterceptorFilter(filterTrace()),
+func getOtelGrpcStatsHandler() stats.Handler {
+	return otelgrpc.NewServerHandler(
+		// nolint:staticcheck // https://github.com/open-telemetry/opentelemetry-go-contrib/issues/4575
+		otelgrpc.WithInterceptorFilter(filters.Not(filters.HealthCheck())),
 		otelgrpc.WithPropagators(otel.GetTextMapPropagator()),
 		otelgrpc.WithMeterProvider(kybermetric.Provider()),
 		otelgrpc.WithTracerProvider(kybertracer.Provider()),
-	}
+	)
 }
 
 func shutdownKyberTrace() {
