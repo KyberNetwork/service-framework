a21d0e1fefa7bc938765f495a4c45f058659c6bb
Phu Ngo <12547020+NgoKimPhu@users.noreply.github.com>
2024-01-09 16:37:42 +0700

    feat: support setting custom http transport configs

--- pkg/client/http.go
+++ pkg/client/http.go
@@ -1,8 +1,6 @@
 package client
 
 import (
-	"net/http"
-
 	"github.com/KyberNetwork/kutils"
 	"github.com/KyberNetwork/kyber-trace-go/pkg/tracer"
 	"github.com/go-resty/resty/v2"
@@ -15,11 +13,8 @@ type HttpCfg struct {
 }
 
 func (*HttpCfg) OnUpdate(_, new *HttpCfg) {
-	if tracer.Provider() != nil {
-		if new.HttpClient == nil {
-			new.HttpClient = &http.Client{}
-		}
-		new.HttpClient.Transport = otelhttp.NewTransport(new.HttpClient.Transport)
-	}
 	new.C = new.NewRestyClient()
+	if tracer.Provider() != nil {
+		new.C.SetTransport(otelhttp.NewTransport(new.C.GetClient().Transport))
+	}
 }
